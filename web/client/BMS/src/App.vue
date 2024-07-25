f5fb49356a025bedbed681a7ce217f9f539a5460
Bimnet <gustesbi@student.gu.se>
2024-07-25 20:22:45 +0200

    #3 Add vue router to the website

--- web/client/BMS/src/App.vue
+++ web/client/BMS/src/App.vue
@@ -1,15 +1,49 @@
 <template>
 
+    <div id="app">
+        <nav>
+            <router-link to="/">Home</router-link>
+        </nav>
+        <router-view></router-view>
+    </div>
+
 </template>
 
 
 <script>
 
-
+export default {
+  name: 'App'
+}
 
 </script>
 
 
-<style scoped>
+<style>
 
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh; /* Ensure the app takes the full viewport height */
+}
+
+nav {
+  display: flex;
+  margin-top: 20px;
+}
+
+nav a {
+  text-decoration: none;
+  color: orange;
+}
+
+a.router-link-active {
+  font-weight: bold;
+}
 </style>
---
+++ web/client/BMS/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+
+<script>
+
+export default {
+  name: 'HomeView'
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
